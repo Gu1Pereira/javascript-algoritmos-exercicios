6d5c10bcf4af98d50557c00db382b2f019663481
Gu1Pereira <guilhermemap85@gmail.com>
2023-12-04 21:51:04 -0300

    atividade com interação com DOM

---
+++ atividade/app.js
@@ -0,0 +1,12 @@
+function jedi(){
+    document.getElementById("mensagem").innerText = "VENHA PARA O LADO DA LUZ"
+    document.getElementById("frase").innerText = "Guerras Não Fazem Ninguém Maior"
+    document.getElementById("imagem").src="jedi.gif"
+
+}
+function sith(){
+    document.getElementById("mensagem").innerText = "VENHA PARA O LADO SOMBRIO, VAI TER BOLO!🎂"
+    document.getElementById("frase").innerText = "A paz é uma mentira, só a emoção importa"
+    document.getElementById("imagem").src="sith.gif"
+ 
+}
--- exercício 07/app.js
+++ exercício 07/app.js
@@ -1,4 +1,10 @@
 function fahrenheit(){
-    let fahrenheit
+    let fahrenheit = parseFloat(document.getElementById("fahrenheit").value)
+    resultado = (fahrenheit - 32) /1.8
+
+    document.getElementById("resultado").innerText = " fahrenheit convertido em celsiues é: " + resultado;
+
     
+
+
 }
