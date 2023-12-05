f5a5080cd23e298c4b89c8c3121ec0c5ec03bbaf
Gu1Pereira <guilhermemap85@gmail.com>
2023-12-04 22:20:44 -0300

    atividade 02

--- atividade/app.js
+++ atividade/app.js
@@ -2,11 +2,14 @@ function jedi(){
     document.getElementById("mensagem").innerText = "VENHA PARA O LADO DA LUZ"
     document.getElementById("frase").innerText = "Guerras Não Fazem Ninguém Maior"
     document.getElementById("imagem").src="jedi.gif"
+    document.body.style="background: #607479" 
 
 }
 function sith(){
     document.getElementById("mensagem").innerText = "VENHA PARA O LADO SOMBRIO, VAI TER BOLO!🎂"
     document.getElementById("frase").innerText = "A paz é uma mentira, só a emoção importa"
     document.getElementById("imagem").src="sith.gif"
+    document.body.style="background: #4f5055"
+
  
 }
