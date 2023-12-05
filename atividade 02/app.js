19d199c5ede29144bb86d2505df75e8a9bd5984c
Gu1Pereira <guilhermemap85@gmail.com>
2023-12-05 20:18:59 -0300

    atividade 2

--- atividade 02/app.js
+++ atividade 02/app.js
@@ -1,13 +1,13 @@
 function skillet(){
     document.getElementById("mensagem").innerText = "UNDEFEATED"
-    document.getElementById("frase").innerText = "5 de agosto 2016"
+    document.getElementById("frase").innerText = "Data de lançamento: 5 de agosto 2016"
     document.getElementById("imagem").src="skillet.png"
     document.body.style="background: #607479" 
 
 }
 function linkin_park(){
     document.getElementById("mensagem").innerText = "IN THE END"
-    document.getElementById("frase").innerText = "9 de outubro 2001"
+    document.getElementById("frase").innerText = "Data de lançamento: 9 de outubro 2001"
     document.getElementById("imagem").src="linkin park.jpg"
     document.body.style="background: #4f5055"
 }
